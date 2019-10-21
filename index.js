e49fb295639ff858c8cff7e8d12dc3c0d41abbab
SaturdayAM <jwl62@georgetown.edu>
2019-10-21 11:46:26 -0400

    organizing with before and after

--- index.js
+++ index.js
@@ -3,7 +3,26 @@ let plays = require('./assets/plays');
 
 
 function statement(invoice, plays){
+	let totalAmount = 0;
+	let volumeCredits = 0;
+	let result = `Statement for ${invoice.customer}\n`;
+	const format = new Intl.NumberFormat("enUS",
+						{ style: "currency", currency: "USD",
+						minimumFractionDigits: 2 }).format;
 
+	for(let perf of invoice.performances){
+        volumeCredits += volumeCreditsFor(perf);
+
+        // print line for this order
+        result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+        totalAmount += amountFor(perf);
+	}
+    result += `Amount owed is ${format(totalAmount / 100)}\n`;
+    result += `You earned ${volumeCredits} credits\n`;
+
+    return result;
+
+    // Helper methods, some require access to scoped var plays
 	function amountFor(aPerformance){
 		let result = 0;
 		switch(playFor(aPerformance).type){
@@ -30,27 +49,12 @@ function statement(invoice, plays){
 		return plays[aPerformance.playID]
 	}
 
-	let totalAmount = 0;
-	let volumeCredits = 0;
-	let result = `Statement for ${invoice.customer}\n`;
-	const format = new Intl.NumberFormat("enUS",
-						{ style: "currency", currency: "USD",
-						minimumFractionDigits: 2 }).format;
-
-	for(let perf of invoice.performances){
-		let thisAmount = amountFor(perf);
-        // add volume credits
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
-        // print line for this order
-        result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
-        totalAmount += thisAmount;
+	function volumeCreditsFor(aPerformance){
+		let result = 0;
+		result += Math.max(aPerformance.audience - 30, 0);
+		if("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience/5);
+		return result;
 	}
-    result += `Amount owed is ${format(totalAmount / 100)}\n`;
-    result += `You earned ${volumeCredits} credits\n`;
-
-    return result;
 }
 
 console.log(statement(invoices[0], plays));
